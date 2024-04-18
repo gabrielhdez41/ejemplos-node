5616f2824475ca41ca153ee058c908e2a9df4635
gabrielhdez41 <gabriel.hernangebt95@gmail.com>
2024-04-17 23:15:21 -0400

    avances curso dia 2

--- feedreader.mjs
+++ feedreader.mjs
@@ -1,7 +1,45 @@
 import { getLinks, saveLinks } from './feed-manager.mjs';
+import { question, close } from './rl.mjs';
 
 const feeds = await getLinks();
 
-feeds.push('http://wwww.tutsplus.com');
+let input = await question('Enter command (list, add, del, read, quit): ');
+
+while(input != 'quit'){
+    let cmdParts = input .trim().split(' ');
+    let cmd = cmdParts[0];
+
+    if(cmd === 'list'){
+        feeds.forEach((url, index) => console.log(`${index}\t${url}`));
+    }
+
+    if(cmd === 'add'){
+        if(cmdParts.length < 2){
+            console.log('Please include the URL with the add command.');
+        }else{
+            feeds.push(cmdParts[1]);
+            console.log(feeds);
+        }
+    }
+
+    if(cmd === 'del'){
+        if(cmdParts.length < 2){
+            console.log('Please include the index of the URL to delete.');
+        }else{
+            let index = parseInt(cmdParts[1], 10);
+
+            if(index > -1 && index < feeds.length){
+                feeds.splice(index, 1);
+            }else{
+                console.log('The provided index is out of range.');
+            }
+        }
+    }
+
+    input = await question('Enter command (list, add, del, read, quit): ');
+}
+
+//feeds.push('http://wwww.tutsplus.com');
 
 await saveLinks(feeds);
+close();
